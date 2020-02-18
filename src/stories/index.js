029d4d450103fcde5c67fc668ceefeaaffc6fd0f
shkumagai <take.this.2.your.grave@gmail.com>
2020-02-19 00:49:22 +0900

    initial commit

---
+++ src/stories/index.js
@@ -0,0 +1,8 @@
+import { storiesOf } from "@storybook/vue";
+
+import MyButton from "./../components/MyButton.vue";
+
+storiesOf("MyButton", module).add("simple", () => ({
+  component: { MyButton },
+  template: `<MyButton>KEEP IT SIMPE</MyButton>`
+}));
