8cddf059ba63f351a44be80d5d430ecc5effeca6
shkumagai <take.this.2.your.grave@gmail.com>
2020-02-19 01:02:31 +0900

    Fix typo

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,5 +4,5 @@ import MyButton from "./../components/MyButton.vue";
 
 storiesOf("MyButton", module).add("simple", () => ({
   component: { MyButton },
-  template: `<MyButton>KEEP IT SIMPE</MyButton>`
+  template: `<MyButton>KEEP IT SIMPLE</MyButton>`
 }));
